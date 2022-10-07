49c269e8d1aedfbd8ecd4452ef9395a4bd9163c9
Markus Deutschmann <Markus.Deutschmann@gfk.com>
2022-10-07 13:39:55 +0200

    fixed win build script

--- build.bat
+++ build.bat
@@ -2,10 +2,12 @@
 if not exist build mkdir build
 set GOOS=windows
 set GOARCH=amd64
-go build -o build/ssh2plink_64bit.exe
-echo created: build/ssh2plink_64bit.exe
+set dest=build/ssh2plink_64bit.exe
+go build -o %dest%
+echo created: %dest%
 
 set GOOS=windows
 set GOARCH=386
-go build -o build/ssh2plink_32bit.exe
-echo created: build/ssh2plink_64bit.exe
+set dest=build/ssh2plink_32bit.exe
+go build -o %dest%
+echo created: %dest%
